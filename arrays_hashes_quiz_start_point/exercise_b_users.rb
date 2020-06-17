a2dac8c488611946c288e153730c107b15e84c21
peaky76 <robertjamespeacock@gmail.com>
2020-06-17 16:23:41 +0100

    erik lottery nums

--- arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -61,6 +61,8 @@ p users["Jonathan"][:twitter]
 p users["Erik"][:home_town]
 
 # 3. Get the array of Erik's lottery numbers
+p users["Erik"][:lottery_numbers]
+
 # 4. Get the species of Avril's pet Monty
 # 5. Get the smallest of Erik's lottery numbers
 # 6. Return an array of Avril's lottery numbers that are even
