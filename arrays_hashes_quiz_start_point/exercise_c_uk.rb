0f2bc5feadf88378295e0f9adc5fdc085f95048b
peaky76 <robertjamespeacock@gmail.com>
2020-06-17 16:50:11 +0100

    print country names

--- arrays_hashes_quiz_start_point/exercise_c_uk.rb
+++ arrays_hashes_quiz_start_point/exercise_c_uk.rb
@@ -33,6 +33,10 @@ northern_ireland = {
 united_kingdom.push(northern_ireland)
 
 # 3. Use a loop to print the names of all the countries in the UK.
+for country in united_kingdom
+  p country[:name]
+end
+
 # 4. Use a loop to find the total population of the UK.
 
 p united_kingdom
