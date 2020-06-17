b29271f6262aec11ed27c6dc0b44d0582758c79d
peaky76 <robertjamespeacock@gmail.com>
2020-06-17 16:38:50 +0100

    erik extra num and hometown

--- arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -85,6 +85,12 @@ end
 p even_nums(users["Avril"][:lottery_numbers])
 
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+users["Erik"][:lottery_numbers].push(7)
+
 # 8. Change Erik's hometown to Edinburgh
+users["Erik"][:home_town] = "Edinburgh"
+
+p users["Erik"][:home_town]
+
 # 9. Add a pet dog to Erik called "Fluffy"
 # 10. Add another person to the users hash
