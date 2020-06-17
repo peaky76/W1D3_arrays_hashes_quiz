309162e205c0c83b91b7f9b93af5523ed43be938
peaky76 <robertjamespeacock@gmail.com>
2020-06-17 16:27:05 +0100

    avril pet species

--- arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -64,6 +64,8 @@ p users["Erik"][:home_town]
 p users["Erik"][:lottery_numbers]
 
 # 4. Get the species of Avril's pet Monty
+p users["Avril"][:pets][0][:species]
+
 # 5. Get the smallest of Erik's lottery numbers
 # 6. Return an array of Avril's lottery numbers that are even
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
